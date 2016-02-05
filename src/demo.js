cc6d0fa91da450f7c1cf2b5900f467f7e497a6e3
Per-Olov Jernberg <po@spotify.com>
2016-02-05 21:15:45 +0100

    Temp

--- src/demo.js
+++ src/demo.js
@@ -29,29 +29,108 @@ var T = 0;
 
 
 */
+var uvb = 0;
+var vtxb = 0;
 
-var nstars = 10000;
+var D = [
+  [0,0,0],
+  [1,0,0],
+  [1,1,0],
+  [0,0,0],
+  [1,1,0],
+  [0,1,0],
+
+  [1,0,1],
+  [0,0,1],
+  [0,1,1],
+  [1,0,1],
+  [0,1,1],
+  [1,1,1],
+
+  [0,0,0],
+  [1,0,0],
+  [1,0,1],
+  [0,0,0],
+  [1,0,1],
+  [0,0,1],
+
+  [1,1,0],
+  [0,1,0],
+  [0,1,1],
+  [1,1,0],
+  [0,1,1],
+  [1,1,1],
+
+  [0,0,0],
+  [0,1,0],
+  [0,1,1],
+  [0,0,0],
+  [0,1,1],
+  [0,0,1],
+
+  [1,1,0],
+  [1,0,0],
+  [1,0,1],
+  [1,1,0],
+  [1,0,1],
+  [1,1,1],
+];
+
+var nstars = 9300;
 var vertices = [];
+var ntris = 0;
+var uvs = [];
 
 var RA = Math.random;
-for(var k=0; k<nstars; k++) {
-  var x = RA() * 2 - 1; // 1.0 * Math.sin(k / 15.0);
-  var y = RA() * 2 - 1; // 1.0 * Math.cos(k / 17.0);
-  var z = RA() * 2 - 1; // 1.0 * Math.cos(k / 17.0);
-  var xx = RA() * 0.2 - 0.1; // 1.0 * Math.cos(k / 17.0);
-  var yy = RA() * 0.2 - 0.1; // 1.0 * Math.cos(k / 17.0);
-  var zz = RA() * 0.2 - 0.1; // 1.0 * Math.cos(k / 17.0);
-  vertices = vertices.concat([x-xx, y-yy, z+zz,
-                              x-yy, y+zz, z-zz,
-                              x+xx, y+xx, z+yy]);
+
+function BLOCK(x,y,z) {
+  D.forEach(function(d) {
+    vertices.push(x + d[0]);
+    vertices.push(y + d[1]);
+    vertices.push(z + d[2]);
+  //  vertices = vertices.concat([x+D[0], y+D[1], z+D[2],
+  //                              x+D[3], y+D[4], z+D[5],
+  //                              x+D[6], y+D[7], z+D[8]]);
+    uvs.push(d[0]);
+    uvs.push(d[1]);
+    uvs.push(d[2]);
+
+    ntris ++;
+  });
 }
+
+for(var z=-15; z<=15; z+=1) {
+  for(var y=-15; y<=15; y+=1) {
+    for(var x=-15; x<=15; x+=1) {
+      var R = Math.sqrt(x*x + y*y + z*z);
+      if (R > (16.0 - 5.0 * Math.random())) {
+        BLOCK(x,y,z);
+      }
+    }
+  }
+}
+
+/*
+for(var k=0; k<nstars; k++) {
+  var x = RA() * 40 - 20; // 1.0 * Math.sin(k / 15.0);
+  var y = RA() * 40 - 20; // 1.0 * Math.cos(k / 17.0);
+  var z = RA() * 40 - 20; // 1.0 * Math.cos(k / 17.0);
+  x = Math.round(x);
+  y = Math.round(y);
+  z = Math.round(z);
+  // var xx = RA() * 0.2 - 0.1; // 1.0 * Math.cos(k / 17.0);
+  // var yy = RA() * 0.2 - 0.1; // 1.0 * Math.cos(k / 17.0);
+  // var zz = RA() * 0.2 - 0.1; // 1.0 * Math.cos(k / 17.0);
+  BLOCK(x,y,z);
+}
+*/
 
 var rf = function() {
 
-  g.clearColor(0.2, 0.4, 0.3, 1);
-  // g.enable(g.DEPTH_TEST);
-  // g.depthFunc(g.LEQUAL);
-  g.clear( 16384/*g.COLOR_BUFFER_BIT*/ );
+  g.clearColor(1, 1, 1, 1);
+   g.enable(g.DEPTH_TEST);
+   g.depthFunc(g.LEQUAL);
+  g.clear( 16384 | g.DEPTH_BUFFER_BIT /*g.COLOR_BUFFER_BIT*/ );
   //  g.useProgram(prog);
 
   /*
@@ -64,11 +143,16 @@ var rf = function() {
   }
   */
 
+  g.bindBuffer(34962/*g.ARRAY_BUFFER*/, vtxb);
   g.bufferData(34962/*g.ARRAY_BUFFER*/, new Float32Array(vertices), 35044/*g.STATIC_DRAW*/);
+
+  g.bindBuffer(34962/*g.ARRAY_BUFFER*/, uvb);
+  g.bufferData(34962/*g.ARRAY_BUFFER*/, new Float32Array(uvs), 35044/*g.STATIC_DRAW*/);
+
   // console.log(g.FLOAT);
   // console.log(g.LINES);
   g.uniform1f(timeUniform, T);
-  g.drawArrays(g.TRIANGLES, 0, nstars);
+  g.drawArrays(g.TRIANGLES, 0, ntris);
 
   requestAnimationFrame(rf);
   T += 0.01;
@@ -100,13 +184,23 @@ g.linkProgram(prog);
 //   alert("Unable to initialize the shader program.");
 // }
 
+g.enable(g.DEPTH_TEST);
+
 g.useProgram(prog);
 vertexPositionAttribute = g.getAttribLocation(prog, "V");
+uvPositionAttribute = g.getAttribLocation(prog, "U");
 timeUniform = g.getUniformLocation(prog, "T");
 
-g.bindBuffer(34962/*g.ARRAY_BUFFER*/, g.createBuffer());
+vtxb = g.createBuffer();
+uvb = g.createBuffer();
+
+g.bindBuffer(34962/*g.ARRAY_BUFFER*/, vtxb);
 g.vertexAttribPointer(vertexPositionAttribute, 3, 5126/*g.FLOAT*/, false, 0, 0);
 g.enableVertexAttribArray(vertexPositionAttribute);
+
+g.bindBuffer(34962/*g.ARRAY_BUFFER*/, uvb);
+g.vertexAttribPointer(uvPositionAttribute, 3, 5126/*g.FLOAT*/, false, 0, 0);
+g.enableVertexAttribArray(uvPositionAttribute);
 
 //
 //
--- src/vertexshader.fs
+++ src/vertexshader.fs
@@ -2,11 +2,84 @@
 // uniform float T;
 
 attribute vec3 V;
+attribute vec3 U;
 varying float Z;
+varying vec3 UV;
+
+float C,S;
+
+mat4 view_frustum(float tan_angle_of_view, float aspect_ratio, float z_near, float z_far) {
+    return mat4(
+        vec4(1.0/tan_angle_of_view,            0.0, 0.0, 0.0),
+        vec4(0.0, aspect_ratio/tan_angle_of_view,   0.0, 0.0),
+        vec4(0.0, 0.0,    (z_far+z_near)/(z_far-z_near), 1.0),
+        vec4(0.0, 0.0, -2.0*z_far*z_near/(z_far-z_near), 0.0)
+    );
+}
+
+/*
+mat4 scale(float x, float y, float z)
+{
+    return mat4(
+        vec4(x,   0.0, 0.0, 0.0),
+        vec4(0.0, y,   0.0, 0.0),
+        vec4(0.0, 0.0, z,   0.0),
+        vec4(0.0, 0.0, 0.0, 1.0)
+    );
+}
+*/
+
+mat4 translate(float x, float y, float z)
+{
+    return mat4(
+        vec4(1.0, 0.0, 0.0, 0.0),
+        vec4(0.0, 1.0, 0.0, 0.0),
+        vec4(0.0, 0.0, 1.0, 0.0),
+        vec4(x,   y,   z,   1.0)
+    );
+}
+
+/*
+mat4 rotate_x(float theta)
+{
+	C = cos(theta);
+	S = sin(theta);
+    return mat4(
+        vec4(1.0, 0.0, 0.0, 0.0),
+        vec4(0.0,   C,   S, 0.0),
+        vec4(0.0,  -S,   C, 0.0),
+        vec4(0.0, 0.0, 0.0, 1.0)
+    );
+}
+
+mat4 rotate_y(float theta)
+{
+	C = cos(theta);
+	S = sin(theta);
+    return mat4(
+        vec4(1.0, 0.0, 0.0, 0.0),
+        vec4(  C, 1.0,   S, 0.0),
+        vec4(0.0, 0.0, 1.0, 0.0),
+        vec4( -S, 0.0,   C, 1.0)
+    );
+}
+*/
+mat4 rotationMatrix(vec3 axis, float angle)
+{
+    axis = normalize(axis);
+    S = sin(angle);
+    C = cos(angle);
+    float oc = 1.0 - C;
+
+    return mat4(oc * axis.x * axis.x + C,           oc * axis.x * axis.y - axis.z * S,  oc * axis.z * axis.x + axis.y * S,  0.0,
+                oc * axis.x * axis.y + axis.z * S,  oc * axis.y * axis.y + C,           oc * axis.y * axis.z - axis.x * S,  0.0,
+                oc * axis.z * axis.x - axis.y * S,  oc * axis.y * axis.z + axis.x * S,  oc * axis.z * axis.z + C,           0.0,
+                0.0,                                0.0,                                0.0,                                1.0);
+}
 
 void main(void) {
 
-//  mat4 P2=mat4( 0.463,  0.0,    0.0,   0.0, 
+//  mat4 P2=mat4( 0.463,  0.0,    0.0,   0.0,
 //                0.0,   0.617, 0.0,  0.0,
 //                0.0,   0.0,   -1.0, -1.0,
 //                sin(T/2.0), sin(T/3.0), 0.0, 1.0);
@@ -21,12 +94,24 @@ void main(void) {
 
  // mat4 P=;
 
-  vec4 v=vec4(V,O);
+  vec4 v=vec4(V, O);
+/*
+  v.z -= 24.0;
+  v.z += 1.0*(sin(T*2.0+v.x)+cos(T*2.0+v.y*1.5));
 
-  v.z -= 14.0;
-  v.z += 5.0*(sin(T*2.0+v.x)+cos(T*2.0+v.y*1.5));
+  mat4 P2=mat4( 6.463,  0.0,    0.0,   0.0,
+                0.0,   6.617, 0.0,  0.0,
+                0.0,   0.0,   -5.0, -1.0,
+                sin(T/2.0), cos(T/3.0), 0.0, 1.0);
 
-  gl_Position=mat4(7, N, N, N, N, 7, N, N, N, N,-O, -O, 3.0*sin(T/3.0), 3.0*sin(T/5.0), N, O)*v;
+  P2 = mat4(7, N, N, N, N, 7, N, N, N, N,-O, -O, 3.0*sin(T/3.0), 3.0*sin(T/5.0), N, O);
+*/
+  gl_Position = view_frustum(tan(0.6), 4.0/3.0, 0.1, 135.0)
+  			  * translate(6.0 * cos(T * 0.3), 6.0 * cos(T * 0.4), 6.0 * sin(T * 0.3))
+  			  * rotationMatrix(vec3(cos(T * 0.5), sin(T * 0.4), cos(T * 0.2)), 1.0 * T)
+  			  // * rotate_x(T)
+  			  * v;//* rotate_x(T) * v;
 
-  Z=O-gl_Position.z/15.0;
+  Z = gl_Position.z / 50.0;
+  UV = U;
 }
